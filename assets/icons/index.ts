8de8384b577889c4924be8f08337ce49615ac180
MACos <macos@MACoss-MacBook-Pro.local>
2026-02-04 13:24:01 +0700

    feat: Implement history tracking and playback, enhance settings with new sections and components, and update app icon assets.

---
+++ assets/icons/index.ts
@@ -0,0 +1,69 @@
+export const iconRegistry = {
+  /** How it display
+   *
+   * ![Back](assets/icons/back.png)
+   * */
+  back: require("./back.png"),
+  /** How it display
+   *
+   * ![Bell](assets/icons/bell.png)
+   * */
+  bell: require("./bell.png"),
+  /** How it display
+   *
+   * ![CaretLeft](assets/icons/caretLeft.png)
+   * */
+  caretLeft: require("./caretLeft.png"),
+  /** How it display
+   *
+   * ![CaretRight](assets/icons/caretRight.png)
+   * */
+  caretRight: require("./caretRight.png"),
+  /** How it display
+   *
+   * ![Check](assets/icons/check.png)
+   * */
+  check: require("./check.png"),
+  /** How it display
+   *
+   * ![Hidden](assets/icons/hidden.png)
+   * */
+  hidden: require("./hidden.png"),
+  /** How it display
+   *
+   * ![Ladybug](assets/icons/ladybug.png)
+   * */
+  ladybug: require("./ladybug.png"),
+  /** How it display
+   *
+   * ![Lock](assets/icons/lock.png)
+   * */
+  lock: require("./lock.png"),
+  /** How it display
+   *
+   * ![Menu](assets/icons/menu.png)
+   * */
+  menu: require("./menu.png"),
+  /** How it display
+   *
+   * ![More](assets/icons/more.png)
+   * */
+  more: require("./more.png"),
+  /** How it display
+   *
+   * ![Settings](assets/icons/settings.png)
+   * */
+  settings: require("./settings.png"),
+  /** How it display
+   *
+   * ![View](assets/icons/view.png)
+   * */
+  view: require("./view.png"),
+  /** How it display
+   *
+   * ![X](assets/icons/x.png)
+   * */
+  x: require("./x.png"),
+}
+
+export type IconTypes = keyof typeof iconRegistry
---
+++ assets/images/index.ts
@@ -0,0 +1,59 @@
+export const imageRegistry = {
+  /** How it display
+   *
+   * ![AppIconAll](assets/images/app-icon-all.png)
+   * */
+  appIconAll: require("./app-icon-all.png"),
+  /** How it display
+   *
+   * ![AppIconAndroidAdaptiveBackground](assets/images/app-icon-android-adaptive-background.png)
+   * */
+  appIconAndroidAdaptiveBackground: require("./app-icon-android-adaptive-background.png"),
+  /** How it display
+   *
+   * ![AppIconAndroidAdaptiveForeground](assets/images/app-icon-android-adaptive-foreground.png)
+   * */
+  appIconAndroidAdaptiveForeground: require("./app-icon-android-adaptive-foreground.png"),
+  /** How it display
+   *
+   * ![AppIconAndroidBackground](assets/images/app-icon-android-background.png)
+   * */
+  appIconAndroidBackground: require("./app-icon-android-background.png"),
+  /** How it display
+   *
+   * ![AppIconAndroidForeground](assets/images/app-icon-android-foreground.png)
+   * */
+  appIconAndroidForeground: require("./app-icon-android-foreground.png"),
+  /** How it display
+   *
+   * ![AppIconAndroidLegacy](assets/images/app-icon-android-legacy.png)
+   * */
+  appIconAndroidLegacy: require("./app-icon-android-legacy.png"),
+  /** How it display
+   *
+   * ![AppIconIos](assets/images/app-icon-ios.png)
+   * */
+  appIconIos: require("./app-icon-ios.png"),
+  /** How it display
+   *
+   * ![AppIconWebFavicon](assets/images/app-icon-web-favicon.png)
+   * */
+  appIconWebFavicon: require("./app-icon-web-favicon.png"),
+  /** How it display
+   *
+   * ![Logo](assets/images/logo.png)
+   * */
+  logo: require("./logo.png"),
+  /** How it display
+   *
+   * ![SadFace](assets/images/sad-face.png)
+   * */
+  sadFace: require("./sad-face.png"),
+  /** How it display
+   *
+   * ![WelcomeFace](assets/images/welcome-face.png)
+   * */
+  welcomeFace: require("./welcome-face.png"),
+}
+
+export type ImagesKeyTypes = keyof typeof imageRegistry
